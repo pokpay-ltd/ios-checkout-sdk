9f56522cee85e5b2544563a7ec68455d0497a560
tgazikas <tgazikas@rpay.ai>
2023-04-04 22:52:08 +0300

    add sdk to package

---
+++ Sources/POK.xcframework/ios-arm64/POK.framework/Headers/POK.h
@@ -0,0 +1,18 @@
+//
+//  POK.h
+//  POK
+//
+//  Created by admin on 27.07.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for POK.
+FOUNDATION_EXPORT double POKVersionNumber;
+
+//! Project version string for POK.
+FOUNDATION_EXPORT const unsigned char POKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POK/PublicHeader.h>
+
+
--- Sources/POKCheckoutSDK/POKCheckoutSDK.swift
+++
@@ -1,6 +0,0 @@
-public struct POKCheckoutSDK {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
