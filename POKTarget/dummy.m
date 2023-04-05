cbf79e072659743dcb48fa8f5aebfb322e62bff4
tgazikas <tgazikas@rpay.ai>
2023-04-05 20:34:12 +0300

    create pok target folder

---
+++ POKTarget/dummy.m
@@ -0,0 +1,8 @@
+//
+//  dummy.m
+//  
+//
+//  Created by thomas on 5/4/23.
+//
+
+#import <Foundation/Foundation.h>
---
+++ POKTarget/include/dummy.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  
+//
+//  Created by thomas on 5/4/23.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
